7e0b2a2a6ecf4bc27b8d09aa93cd6cd98639a2c2
Pavel Lytkin <Pavel.Lytkin@evraz.com>
2024-11-29 12:01:00 +0300

    поднята версия до 0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='classic-migrations',
-    version='0.0.5',
+    version='0.0.6',
     description='Database migrations with SQL',
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
