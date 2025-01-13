f6b4940fdbb8d0e244989ca781984722d73a9719
kazakovtsev_nm <Nikolay.Kazakovtsev@evraz.com>
2025-01-13 16:59:55 +0700

    Исправлена ошибка в имени переменных окружения при формировании адреса базы данных

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='classic-migrations',
-    version='0.0.11',
+    version='0.0.12',
     description='Database migrations with SQL',
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
